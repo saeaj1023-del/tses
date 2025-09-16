706b954c750d94db077bbf57ecb737f476cfd8b5
saeaj1023-del <saeaj1023@gmail.com>
2025-09-16 10:56:34 +0900

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -6,15 +6,13 @@ int main()
 {
 	int aatc = 0;
 	// °è»ê½Ä : + - * / %
-	int HP = 100;
-	int Mosnteratk = 20;
-	int DEFENCE = 10; 
-	int damage = HP - DEFENCE;
-	int hp = hp - damage;
-	cout << HP;
+	int MentalPoint = 100;
+	int Mesmerism = 20; 
+	int Mentality = MentalPoint - mesmerism;
+	cout << Mentality;
 
-		if (hp <= 0)
-			cout << "Game Over" << endl;
+		if (Mentality <= 0
+			cout << "Game Over" << endl;)
 
 	return 0;
 }
