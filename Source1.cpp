e2c68e18da6e4ad8bebaefc17a6847f0a7b7056d
saeaj1023-del <saeaj1023@gmail.com>
2025-09-16 10:42:22 +0900

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-	std cout << "rk" << std::endl;
+	int aatc = 0;
+	// °è»ê½Ä : + - * / %
+	int HP = 100;
+	int Mosnteratk = 20;
+	int DEFENCE = 10; 
+	int damage = HP - DEFENCE;
+	int hp = hp - damage;
+	cout << HP;
+
+		if (hp <= 0)
+			cout << "Game Over" << endl;
+
 	return 0;
 }
